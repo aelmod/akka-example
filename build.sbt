c6b251d906bd8d2610ff22a9973cba8b2aff7655
yevheniishc <yevheniishc@softwareplanet.uk.com>
2018-08-15 00:00:40 +0300

    Add use api for translation

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "0.1"
 scalaVersion := "2.12.6"
 
 libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.5.14"
+libraryDependencies += "com.typesafe.akka" %% "akka-http" % "10.1.3"
+libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.14"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,36 +1,57 @@
-import akka.actor.{Actor, ActorSystem, Props}
-import akka.pattern.ask
-import akka.util.Timeout
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.pattern.{ask, pipe}
+import akka.stream.ActorMaterializer
+import akka.util.{ByteString, Timeout}
 
-import scala.concurrent.Await
 import scala.concurrent.duration.{Duration, _}
+import scala.concurrent.{Await, Future}
 
 object Main extends App {
-  val actorSystem = ActorSystem("my-actor-system")
+  implicit val actorSystem = ActorSystem("my-actor-system")
   implicit val dispatcher = actorSystem.dispatcher
 
+  implicit val materializer = ActorMaterializer()
 
-  val translation = actorSystem.actorOf(Props[TranslationActor], "translation")
+  val translation = actorSystem.actorOf(Props(classOf[TranslationActor], materializer), "translation")
 
+  implicit val timeout = Timeout(3 seconds)
 
-  implicit val timeout = Timeout(5 seconds)
+  val program = for {
+    t1 <- translation ? "I"
+    t2 <- translation ? "HZ"
+    t3 <- translation ? 123
+    _ <- actorSystem.terminate()
+  } yield (t1, t2, t3)
 
-  translation ? "I" foreach println
-  translation ? "HZ" foreach println
-  translation ? 123 foreach println
-
-
-  Await.ready(actorSystem.terminate(), Duration.Inf)
+  val programRes = Await.result(program, Duration.Inf)
+  println(programRes)
 }
 
-class TranslationActor extends Actor {
-  val translations = Map(
-    "I" -> "Я",
-    "Have" -> "Маю"
-  )
+class TranslationActor(implicit val materializer: ActorMaterializer) extends Actor with ActorLogging {
+  final implicit val system: ActorSystem = context.system
+  final implicit val ec = context.system.dispatcher
+
+  def responseFuture(phrase: String): Future[HttpResponse] =
+    Http()
+      .singleRequest(HttpRequest(
+        uri = "http://api.lingualeo.com/gettranslates?port=1001",
+        method = HttpMethods.POST,
+        entity = FormData("word" -> phrase).toEntity(HttpCharsets.`UTF-8`)
+      ))
 
   override def receive: Receive = {
-    case s: String => sender ! s -> translations.getOrElse(s, "Translation not found")
-    case any => sender ! any.getClass.getName -> "ERROR"
+    case s: String =>
+      responseFuture(s)
+        .flatMap(_.entity.dataBytes.runFold(ByteString(""))(_ ++ _))
+        .map(res => Right(res.decodeString("utf-8")))
+        .recover({
+          case t: Throwable => Left(t.getMessage)
+        })
+        .pipeTo(sender)
+
+    case any =>
+      sender ! Left(any.getClass.getName -> "ERROR")
   }
 }
