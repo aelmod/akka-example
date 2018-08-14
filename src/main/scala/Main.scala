d29bbe9c98519dd80a99828343b063f3b2eaabc1
yevheniishc <yevheniishc@softwareplanet.uk.com>
2018-08-14 22:17:44 +0300

    Add translation feature

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,36 @@
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.pattern.ask
+import akka.util.Timeout
 
 import scala.concurrent.Await
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.{Duration, _}
 
 object Main extends App {
   val actorSystem = ActorSystem("my-actor-system")
-  val translation: ActorRef = actorSystem.actorOf(Props[TranslationActor], "translation")
+  implicit val dispatcher = actorSystem.dispatcher
+
+
+  val translation = actorSystem.actorOf(Props[TranslationActor], "translation")
+
+
+  implicit val timeout = Timeout(5 seconds)
+
+  translation ? "I" foreach println
+  translation ? "HZ" foreach println
+  translation ? 123 foreach println
+
 
   Await.ready(actorSystem.terminate(), Duration.Inf)
 }
 
 class TranslationActor extends Actor {
+  val translations = Map(
+    "I" -> "Я",
+    "Have" -> "Маю"
+  )
+
   override def receive: Receive = {
-    case "sasay" => println("sasayu")
+    case s: String => sender ! s -> translations.getOrElse(s, "Translation not found")
+    case any => sender ! any.getClass.getName -> "ERROR"
   }
 }
